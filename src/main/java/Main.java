bbd5d685adfd0119de390f1c6cb95ead71762c49
antoniooreany <antonio_oreany@mail.ru>
2018-06-05 22:15:08 +0300

    destination folder created

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,20 +7,25 @@ import java.util.HashMap;
  */
 public class Main {
     public static void main(String[] args) {
-        String md5FileName = "docs.md5";
         String folder0Name = "D:\\Old_D — копия\\";
-        String folderName = "docs\\";
+        String md5FileName0 = "docs";
+        String md5FileName1 = ".md5";
+        String md5FileName = md5FileName0 + md5FileName1;
+        String folderName = md5FileName0 + "\\";
         String pathname = folder0Name + folderName + md5FileName;
         HashMap<String, String> hashMap = new HashMap<String, String>();
         long initialLinesCount = LineByLineFileReader.readFileLineByLine(pathname, hashMap);
+        File newFolder = new File(folder0Name + "dest_" + md5FileName0);
+        newFolder.mkdir();
 
         Collection<String> relativePaths = hashMap.values();
         for (String relativePath :
                 relativePaths) {
             System.out.println(relativePath);
-            
+
             File fileSrc = new File(folder0Name + folderName + relativePath);
-            if (fileSrc.renameTo(new File(folder0Name + "new_" + folderName + fileSrc.getName()))) {
+            File fileDest = new File(folder0Name + "dest_" + folderName + relativePath);
+            if (fileSrc.renameTo(fileDest)) {
                 System.out.println("file moved successfully");
             } else {
                 System.out.println("file have not been moved");
